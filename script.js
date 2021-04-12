0c9bb96532febef7a5320d0d09f69abb7550a351
Cynthia Navarro <cynthianavarro93@hotmail.com>
2021-04-12 15:50:13 -0400

    start

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 const canvas = document.querySelector('#canvas');
 let ctx = canvas.getContext('2d');
 
-// ctx.font = "18px Arial";
-// ctx.fillStyle = "white";
-// ctx.fillText('Distraction Avoidance!', 90, 20);
-
