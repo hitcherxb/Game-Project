dfe2b3db78f4b38bbd2490987fe0bdbcd5aebb9b
Cody Thigpen <ckthigpen1@gmail.com>
2021-04-12 15:48:54 -0400

    js

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ let ctx = canvas.getContext('2d');
 // ctx.fillStyle = "white";
 // ctx.fillText('Distraction Avoidance!', 90, 20);
 
+
+let start_image = new Image();
+start_image.src = './images/chalkboardBackground.jpeg';
+
+const background = {
+    x: 0,
+    y: 0,
+    w: canvas.width,
+    h: canvas.height,
+    draw: function () {
+        ctx.drawImage(background_image, this.x, this.y, this.w, this.h);
+    },
+};
+
+function animate() {
+    stopGame = requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    background.draw()
+
+}
+animate();
