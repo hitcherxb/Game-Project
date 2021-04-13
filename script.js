5b2906d9b86e8b4e1a9ee5f52e843a36e4e36e3b
Cynthia Navarro <cynthianavarro93@hotmail.com>
2021-04-13 16:42:28 -0400

    canvas setup

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ const background = {
 let buttons = document.querySelectorAll('button')
 for (button of buttons) {
     button.onclick = function (e) {
-        // console.log(e, this, this.id, this.parentNode.parentNode, this.parentNode.parentNode.nextElementSibling)
 
         this.parentNode.parentNode.parentNode.parentNode.classList.add('hidden')
         this.parentNode.parentNode.parentNode.parentNode.nextElementSibling.classList.remove('hidden')
@@ -36,29 +35,36 @@ for (button of buttons) {
     }
 }
 
-class Char {
-    constructor(img) {
+class Student {
+    constructor(img, x, y, w, h) {
         this.img = img;
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
     }
     draw = () => {
         console.log(this)
-        ctx.drawImage(this.img, 100, 100, 100, 100)
+        ctx.drawImage(this.img, this.x, this.y, this.w, this.h)
     }
 }
 
-let character = new Char()
+let character = new Student(this.img, canvas.width/2, 75, 70, 70)
 function animate() {
     window.requestAnimationFrame(animate)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
     character.draw()
 
 }
 
-// document.querySelector('#startGame').onclick = function () {
-//     this.parentNode.parentNode.classList.add('hidden')
-//     this.parentNode.parentNode.nextElementSibling.classList.remove('hidden')
-// }
+window.onkeydown = function (e) {
+    if (e.key === 'ArrowLeft') {
+        character.x -= 10
+    }
+    if (e.key === 'ArrowRight') {
+        character.x += 10
+    }
+    console.log(this)
+}
 
-
-
-// document.querySelector('#vsimg button').onclick = function () {
-//     document.querySelector('#canvas').classList.toggle('hidden');
+animate()
