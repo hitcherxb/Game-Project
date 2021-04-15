2d15c90dd3f8ea74649cb702b31ffcda504b4802
Cody Thigpen <ckthigpen1@gmail.com>
2021-04-15 12:28:13 -0400

    song

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ canvas.height = 750;
 let player = null
 
 let audio = new Audio('audio/Heavens.mp3')
-
-audio.loop = true;
-audio.volume = 0.08;
-audio.play();
+window.onload = function () {
+    audio.loop = true;
+    audio.volume = 0.08;
+    audio.play();
+}
 
 
 let buttons = document.querySelectorAll('button')
